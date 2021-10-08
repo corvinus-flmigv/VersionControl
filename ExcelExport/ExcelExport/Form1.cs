7f9912a21425d6d34b8410dfb83f30fc7fcc777c
corvinus-flmigv <garaiorsi2000@gmail.com>
2021-10-08 22:54:33 +0200

    Create Excel Workbook

--- ExcelExport/ExcelExport/Form1.cs
+++ ExcelExport/ExcelExport/Form1.cs
@@ -17,16 +17,49 @@ namespace ExcelExport
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> lakasok;
 
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+
         public Form1()
         {
             InitializeComponent();
             LoadData();
             dataGridView1.DataSource = lakasok;
+            CreateExcel();
         }
 
         private void LoadData()
         {
             lakasok = context.Flats.ToList();
+        }
+
+        public void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+                CreateTable(); 
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+
+            catch (Exception ex) 
+            {
+                string hiba = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(hiba, "Error");
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+
+        private void CreateTable()
+        {
+
         }
     }
 }
