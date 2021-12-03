ac82a8694634ecfe7c94aee3f612ca3817cd768b
corvinus-flmigv <garaiorsi2000@gmail.com>
2021-12-03 15:52:49 +0100

    Szimuláció

--- MicroSimExample/MicroSimExample/Form1.cs
+++ MicroSimExample/MicroSimExample/Form1.cs
@@ -26,6 +26,27 @@ namespace MicroSimExample
             Population = GetPopulation(@"C:\Temp\nép-teszt.csv");
             BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
+
+            for (int year = 2005; year <= 2024; year++)
+            {
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    //...
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(string.Format(
+                    "Év: {0}\nFiúk: {1}\nLányok: {2}\n",
+                    year,
+                    nbrOfMales,
+                    nbrOfFemales));
+            }
         }
 
         public List<Person> GetPopulation(string csvpath)
